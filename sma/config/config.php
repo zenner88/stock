8515c2311a4a217827ba7a688832bf4acc3ec78a
Yannri Zenner <zenner88@gmail.com>
2017-12-21 16:05:37 +0700

    revisi stock product

--- sma/config/config.php
+++ sma/config/config.php
@@ -14,7 +14,7 @@
 | path to your installation.
 |
 */
-$config['base_url']	= 'http://localhost/stock/';
+$config['base_url']	= 'http://localhost:88/stock_cafe/';
 
 /*
 |--------------------------------------------------------------------------
